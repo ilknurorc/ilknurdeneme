ee198c72d61c97c18c0a0243d61b596269a3c182
ilknurorc <94916641+ilknurorc@users.noreply.github.com>
2021-11-23 19:49:07 +0300

    Add files via upload

---
+++ ilknur3/src/com/company/Main.java
@@ -0,0 +1,18 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Sayi Giriniz : ");
+        int deger = scan.nextInt();
+        int faktoriyel = 1;
+        for(int i = 1; i<= deger; i++){
+            faktoriyel = faktoriyel * i;
+        }
+
+        System.out.println(faktoriyel);
+    }
+}
